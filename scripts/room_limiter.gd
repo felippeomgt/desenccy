dbae62098ef23382e9b915e0abf012a1f0ee57ac
Iuri Bruski <iuri.leo.bruski@gmail.com>
2025-01-28 22:24:59 -0300

    Camera and room limiter

---
+++ scripts/room_limiter.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var zoom: Vector2 = Vector2(1,1)
+
+@onready var topLeft = $TopLeft
+@onready var bottomRight = $BottomRight
+
+func _ready() -> void:
+	add_to_group("room")
+
+func _on_body_entered(body: Node2D) -> void:
+	var player = get_tree().get_first_node_in_group("player")
+	if player:
+		var playerCamera: Camera2D = player.get_node("Camera2D")
+		player.get_node("Camera2D").limit_top = topLeft.global_position.y
+		player.get_node("Camera2D").limit_left = topLeft.global_position.x
+		player.get_node("Camera2D").limit_bottom = bottomRight.global_position.y
+		player.get_node("Camera2D").limit_right = bottomRight.global_position.x
+		player.get_node("Camera2D").zoom = zoom
